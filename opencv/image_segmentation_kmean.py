0f8fe981e32fae0e88f93ab78ecca83e60121d39
Anayverma <anayverma2147@gmail.com>
2024-01-02 22:32:36 +0530

    draft-socketand multithreading

--- opencv/image_segmentation_kmean.py
+++ opencv/image_segmentation_kmean.py
@@ -3,7 +3,7 @@ import cv2
 from sklearn.cluster import KMeans
 
 # Load the image
-image = cv2.imread('image.png')
+image = cv2.imread('image.pnggit')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
 
 # Flatten the image
